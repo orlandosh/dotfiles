de4a6270d3e1e424cddcdc05727fb0148da8fbbb
orlandosh <84464728+orlandosh@users.noreply.github.com>
2022-04-12 09:19:46 -0300

    remove purple from indent blankline

--- neovim/lua/indent_blankline_cfg.lua
+++ neovim/lua/indent_blankline_cfg.lua
@@ -3,7 +3,6 @@ vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
 vim.opt.list = true
 require("indent_blankline").setup({
 	show_current_context = true,
@@ -14,6 +13,5 @@ require("indent_blankline").setup({
 		"IndentBlanklineIndent3",
 		"IndentBlanklineIndent4",
 		"IndentBlanklineIndent5",
-		"IndentBlanklineIndent6",
 	},
 })
