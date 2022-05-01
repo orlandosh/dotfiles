aff4dfa1259f47f6ec39d9aa4a91643e1b0a6273
orlandosh <84464728+orlandosh@users.noreply.github.com>
2022-05-01 18:30:13 -0300

    add feline colours

--- neovim/lua/plugins/feline/theme.lua
+++ neovim/lua/plugins/feline/theme.lua
@@ -1,5 +1,22 @@
 local M = {}
 
-M.my_theme = { bg = "#3A3845", fg = "#FAF5E4", green = "#A2B38B", red = "#AC7D88", blue = "#A0BCC2" }
+M.my_theme = {
+	bg = "#3A3845",
+	fg = "#FAF5E4",
+
+	black = "#3A3845",
+	blue = "#9ADCFF",
+	cyan = "#D1E8E4",
+	darkblue = "#9ADCFF",
+	green = "#BEDBBB",
+	oceanblue = "#9ADCFF",
+	orange = "#FFA299",
+	magenta = "#FFC7C7",
+	red = "#FF7878",
+	skyblue = "#9ADCFF",
+	violet = "#C3AED6",
+	white = "#FAF5E4",
+	yellow = "#FEE2B3",
+}
 
 return M
