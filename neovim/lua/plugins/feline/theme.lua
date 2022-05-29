677f0221c6e7b3f245821bd2b35882fac5392222
orlandosh <84464728+orlandosh@users.noreply.github.com>
2022-05-29 19:16:59 -0300

    feline gruvbox

--- neovim/lua/plugins/feline/theme.lua
+++ neovim/lua/plugins/feline/theme.lua
@@ -1,22 +1,39 @@
 local M = {}
 
+-- M.my_theme = {
+-- 	bg = "#3A3845",
+-- 	fg = "#FAF5E4",
+--
+-- 	black = "#3A3845",
+-- 	blue = "#9ADCFF",
+-- 	cyan = "#D1E8E4",
+-- 	darkblue = "#9ADCFF",
+-- 	green = "#BEDBBB",
+-- 	oceanblue = "#9ADCFF",
+-- 	orange = "#FFA299",
+-- 	magenta = "#FFC7C7",
+-- 	red = "#FF7878",
+-- 	skyblue = "#9ADCFF",
+-- 	violet = "#C3AED6",
+-- 	white = "#FAF5E4",
+-- 	yellow = "#FEE2B3",
+-- }
+--
 M.my_theme = {
-	bg = "#3A3845",
-	fg = "#FAF5E4",
-
-	black = "#3A3845",
-	blue = "#9ADCFF",
-	cyan = "#D1E8E4",
-	darkblue = "#9ADCFF",
-	green = "#BEDBBB",
-	oceanblue = "#9ADCFF",
-	orange = "#FFA299",
-	magenta = "#FFC7C7",
-	red = "#FF7878",
-	skyblue = "#9ADCFF",
-	violet = "#C3AED6",
-	white = "#FAF5E4",
-	yellow = "#FEE2B3",
+	bg = "#3c3836",
+	fg = "#fbf1c7",
+	black = "#282828",
+	blue = "#83a598",
+	cyan = "#689d6a",
+	darkblue = "#83a598",
+	green = "#98971a",
+	oceanblue = "#83a598",
+	skyblue = "#83a598",
+	orange = "#d79921",
+	magenta = "#b16286",
+	red = "#cc241d",
+	white = "#fbf1c7",
+	yellow = "#fabd2f",
 }
 
 return M
