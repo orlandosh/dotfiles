2d147a84930ac9699fe736af9f7e08634078b6b1
orlandosh <84464728+orlandosh@users.noreply.github.com>
2022-05-31 10:11:36 -0300

    add temp comment to remind me that i have to hide the X on the end of the tabline

--- neovim/lua/settings.lua
+++ neovim/lua/settings.lua
@@ -22,3 +22,37 @@ set.modifiable = true
 set.laststatus = 3
 set.ignorecase = true
 set.spell = true
+
+-- remove X at the end
+-- vim.cmd([[
+-- function! MyTabLine()
+--   let s = ''
+--   for i in range(tabpagenr('$'))
+--     " select the highlighting
+--     if i + 1 == tabpagenr()
+--       let s .= '%#TabLineSel#'
+--     else
+--       let s .= '%#TabLine#'
+--     endif
+--
+--     " set the tab page number (for mouse clicks)
+--     let s .= '%' . (i + 1) . 'T'
+--
+--     " the label is made by MyTabLabel()
+--     let s .= ' %{MyTabLabel(' . (i + 1) . ')} '
+--   endfor
+--
+--   " after the last tab fill with TabLineFill and reset tab page nr
+--   let s .= '%#TabLineFill#%T'
+--
+--   return s
+-- endfunction
+--
+-- function! MyTabLabel(n)
+--   let buflist = tabpagebuflist(a:n)
+--   let winnr = tabpagewinnr(a:n)
+--   return bufname(buflist[winnr - 1])
+-- endfunction
+--
+-- set tabline=%!MyTabLine()
+-- ]])
