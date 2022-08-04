c3ca6e016f665e14c8b49b424eb99fc022e6c1c5
orlandosh <84464728+orlandosh@users.noreply.github.com>
2022-08-04 20:29:37 -0300

    update feline with material colours

--- neovim/lua/plugins/feline/theme.lua
+++ neovim/lua/plugins/feline/theme.lua
@@ -1,5 +1,4 @@
 local M = {}
-
 -- M.my_theme = {
 -- 	bg = "#3A3845",
 -- 	fg = "#FAF5E4",
@@ -20,20 +19,20 @@ local M = {}
 -- }
 --
 M.my_theme = {
-	bg = "#3c3836",
-	fg = "#fbf1c7",
+	bg = "#45403d",
+	fg = "#ddc7a1",
 	black = "#282828",
-	blue = "#83a598",
-	cyan = "#689d6a",
-	darkblue = "#83a598",
-	green = "#98971a",
+	blue = "#7daea3",
+	cyan = "#89b482",
+	darkblue = "#7daea3",
+	green = "#a9b665",
 	oceanblue = "#83a598",
-	skyblue = "#83a598",
+	skyblue = "#7daea3",
 	orange = "#d79921",
-	magenta = "#b16286",
-	red = "#cc241d",
+	magenta = "#e090b4",
+	red = "#ea6962",
 	white = "#fbf1c7",
-	yellow = "#fabd2f",
+	yellow = "#d8a657",
 }
 
 return M
