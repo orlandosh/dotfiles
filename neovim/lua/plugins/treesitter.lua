cab4bc42e34c0ab1aa887917b6a1ab0ef4610720
orlandosh <84464728+orlandosh@users.noreply.github.com>
2023-08-01 16:59:03 -0300

    remove treesitter highlight by default

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ require("nvim-treesitter.configs").setup({
 	auto_install = true,
 
 	highlight = {
-		enable = true,
+		enable = false,
 		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
 		-- Using this option may slow down your editor, and you may see some duplicate highlights.
