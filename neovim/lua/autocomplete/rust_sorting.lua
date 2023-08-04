a9673c0ab1901aaf893ee5758701fa16f52dd3f1
orlandosh <84464728+orlandosh@users.noreply.github.com>
2023-08-04 11:21:20 -0300

    add license to rust_sorting.lua

--- neovim/lua/autocomplete/rust_sorting.lua
+++ neovim/lua/autocomplete/rust_sorting.lua
@@ -1,5 +1,26 @@
--- rust priorization functions come from: https://github.com/ryo33/nvim-cmp-rust/tree/main commit #0c9fafd there's no license, just contact me if you don't want me to use your configs. if you add a license i will add to this file
--- i only changed uh... to make this work without vim.iter
+-- MIT License
+
+-- Copyright (c) 2023 Ryo Hirayama
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
+-- changes to the program: replace .iter() and add IntoSql
 
 local function deprioritize_postfix(entry1, entry2)
 	local kind1 = entry1:get_kind()
