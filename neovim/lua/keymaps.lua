5a48cc42cf1e96f3a1b6a306c2241e7f812aca4a
orlandosh <84464728+orlandosh@users.noreply.github.com>
2023-08-05 12:00:42 -0300

    add lsp_workspace_symbols keymap

--- neovim/lua/keymaps.lua
+++ neovim/lua/keymaps.lua
@@ -15,6 +15,7 @@ vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
 vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
 vim.keymap.set("n", "<leader>fw", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>")
+vim.keymap.set("n", "<leader>fs", "<cmd>Telescope lsp_workspace_symbols<cr>")
 vim.keymap.set("n", "<leader>fd", "<cmd>Telescope lsp_document_symbols<cr>")
 vim.keymap.set("n", "<leader>tr", "<cmd>Telescope diagnostics<cr>") -- from trouble
 vim.keymap.set("n", "<leader>ts", "<cmd>Telescope treesitter<cr>")
