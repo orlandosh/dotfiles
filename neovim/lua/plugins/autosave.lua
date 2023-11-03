97796e387f7b3124569ad07cb3a67044976bdae8
orlandosh <84464728+orlandosh@users.noreply.github.com>
2023-11-03 14:35:10 -0300

    remove custom autosave settings

--- neovim/lua/plugins/autosave.lua
+++ neovim/lua/plugins/autosave.lua
@@ -2,14 +2,5 @@ local autosave = require("auto-save")
 
 autosave.setup({
 	enabled = true,
-	execution_message = {
-		message = function() -- message to print on save
-			return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
-		end,
-		dim = 0.18, -- dim the color of `message`
-		cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-	},
-	trigger_events = { "InsertLeave" },
 	write_all_buffers = true,
-	debounce_delay = 350,
 })
