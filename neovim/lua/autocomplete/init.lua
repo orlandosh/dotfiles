a748b9be62553cb0df90deb83206d15837fe0dfb
orlandosh <84464728+orlandosh@users.noreply.github.com>
2024-04-13 14:50:55 -0300

    setup hover with jedi and use native inlayhints

--- neovim/lua/autocomplete/init.lua
+++ neovim/lua/autocomplete/init.lua
@@ -5,8 +5,6 @@ local on_attach = require("autocomplete.on_attach")(opts)
 local utils = require("utils")
 local util = require("lspconfig/util")
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
 require("autocomplete.cmp")
 
 local bufdir = utils.get_dir()
@@ -16,9 +14,10 @@ local lspconfig = require("lspconfig")
 local installed_servers = require("mason-lspconfig").get_installed_servers()
 
 for _, server in pairs(installed_servers) do
+	local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 	local lsp_opts = { on_attach = on_attach, capabilities = capabilities }
 
-	if server == "pyright" then
+	if server == "basedpyright" then
 		if string.find(bufdir, work_keyword) then
 			lsp_opts.settings = {
 				python = {
@@ -31,7 +30,27 @@ for _, server in pairs(installed_servers) do
 	end
 
 	if server == "jedi_language_server" then
-		lsp_opts.init_options = { workspace = { symbols = { maxSymbols = -1 } } }
+		local get_poetry_env_path = function()
+			local handle = io.popen("poetry env info")
+			if handle == nil then
+				return ""
+			end
+
+			local output = handle:read("*a")
+			handle:close()
+
+			if output:match("Poetry could not find a pyproject.toml file in") then
+				return ""
+			end
+
+			local env_path = output:match("Path:%s+(%S+)")
+
+			return env_path or ""
+		end
+
+		if get_poetry_env_path() ~= "" then
+			lsp_opts.init_options = { workspace = { environmentPath = get_poetry_env_path() } }
+		end
 	end
 
 	if server == "pylsp" then
@@ -39,18 +58,58 @@ for _, server in pairs(installed_servers) do
 			pylsp = {
 				plugins = {
 					jedi_completion = {
-						fuzzy = true,
-						eager = true,
-						include_class_objects = true,
-						include_function_objects = true,
-						resolve_at_most = 500,
+						enabled = false,
+						eager = false,
+					},
+					jedi_hover = {
+						enabled = false,
+						eager = false,
+					},
+					jedi_references = {
+						enabled = false,
+					},
+					jedi_definition = {
+						enabled = false,
+					},
+					jedi_signature_help = {
+						enabled = false,
+					},
+					jedi_symbols = {
+						enabled = false,
+					},
+					mccabe = {
+						enabled = false,
+					},
+					preload = {
+						enabled = false,
+					},
+					pycodestyle = {
+						enabled = false,
+					},
+					pydocstyle = {
+						enabled = false,
+					},
+					pyflakes = {
+						enabled = false,
+					},
+					pylint = {
+						enabled = false,
 					},
 					rope_autoimport = {
-						enabled = true,
+						enabled = false,
+					},
+					yapf = {
+						enabled = false,
+					},
+					autopep8 = {
+						enabled = false,
+					},
+					flake8 = {
+						enabled = false,
 					},
 					rope_completion = {
-						enabled = true,
-						eager = true,
+						enabled = false,
+						eager = false,
 					},
 				},
 			},
@@ -147,109 +206,111 @@ for _, server in pairs(installed_servers) do
 		}
 	end
 
+	if server == "efm" then
+		local settings = {
+			lintDebounce = 1000,
+			languages = {
+				python = {
+					{
+						formatCommand = "black --quiet - | isort --stdout --profile black - | black --quiet -",
+						formatStdin = true,
+					},
+				},
+				lua = {
+					{
+						formatCommand = "stylua -",
+						formatStdin = true,
+					},
+				},
+				vue = {
+					{
+						formatCommand = "bunx prettier --parser vue",
+						formatStdin = true,
+					},
+				},
+				cs = {
+					{
+						formatCommand = "dotnet-csharpier --write-stdout -",
+						formatStdin = true,
+					},
+				},
+				json = {
+					{
+						formatCommand = "bunx prettier --parser json",
+						formatStdin = true,
+					},
+				},
+				html = {
+					{
+						formatCommand = "bunx prettier --parser html",
+						formatStdin = true,
+					},
+				},
+				javascript = {
+					{
+						formatCommand = "bunx prettier --parser babel",
+						formatStdin = true,
+					},
+				},
+				typescript = {
+					{
+						formatCommand = "bunx prettier --parser typescript",
+						formatStdin = true,
+					},
+				},
+				svelte = {
+					{
+						formatCommand = "bunx prettier --parser svelte",
+						formatStdin = true,
+					},
+				},
+				css = {
+					{
+						formatCommand = "bunx prettier --parser css",
+						formatStdin = true,
+					},
+				},
+				scss = {
+					{
+						formatCommand = "bunx prettier --parser scss",
+						formatStdin = true,
+					},
+				},
+			},
+		}
+
+		settings.lintDebounce = 1000
+
+		lspconfig[server].setup({
+			on_attach = on_attach,
+			capabilities = capabilities,
+			settings = settings,
+			filetypes = {
+				"python",
+				"lua",
+				"vue",
+				"cs",
+				"sql",
+				"json",
+				"html",
+				"css",
+				"scss",
+				"javascript",
+				"typescript",
+				"svelte",
+			},
+			init_options = { documentFormatting = true, diagnostics = true },
+			root_dir = function(fname)
+				return not bufdir:find(work_keyword) and util.root_pattern("node_modules")(fname)
+					or not bufdir:find(work_keyword) and util.root_pattern(".git")(fname)
+					or vim.fn.getcwd()
+			end,
+		})
+	end
+
 	if server ~= "efm" then
 		lspconfig[server].setup(lsp_opts)
 	end
 end
-
-local settings = {
-	lintDebounce = 1000,
-	languages = {
-		python = {
-			{
-				formatCommand = "black --quiet - | isort --stdout --profile black - | black --quiet -",
-				formatStdin = true,
-			},
-		},
-		lua = {
-			{
-				formatCommand = "stylua -",
-				formatStdin = true,
-			},
-		},
-		vue = {
-			{
-				formatCommand = "bunx prettier --parser vue",
-				formatStdin = true,
-			},
-		},
-		cs = {
-			{
-				formatCommand = "dotnet-csharpier --write-stdout -",
-				formatStdin = true,
-			},
-		},
-		json = {
-			{
-				formatCommand = "bunx prettier --parser json",
-				formatStdin = true,
-			},
-		},
-		html = {
-			{
-				formatCommand = "bunx prettier --parser html",
-				formatStdin = true,
-			},
-		},
-		javascript = {
-			{
-				formatCommand = "bunx prettier --parser babel",
-				formatStdin = true,
-			},
-		},
-		typescript = {
-			{
-				formatCommand = "bunx prettier --parser typescript",
-				formatStdin = true,
-			},
-		},
-		svelte = {
-			{
-				formatCommand = "bunx prettier --parser svelte",
-				formatStdin = true,
-			},
-		},
-		css = {
-			{
-				formatCommand = "bunx prettier --parser css",
-				formatStdin = true,
-			},
-		},
-		scss = {
-			{
-				formatCommand = "bunx prettier --parser scss",
-				formatStdin = true,
-			},
-		},
-	},
-}
-
-settings.lintDebounce = 1000
-
-require("lspconfig").efm.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	settings = settings,
-	filetypes = {
-		"python",
-		"lua",
-		"vue",
-		"cs",
-		"sql",
-		"json",
-		"html",
-		"css",
-		"scss",
-		"javascript",
-		"typescript",
-		"svelte",
-	},
-	init_options = { documentFormatting = true, diagnostics = true },
-	root_dir = function(fname)
-		return not bufdir:find(work_keyword) and util.root_pattern("node_modules")(fname)
-			or not bufdir:find(work_keyword) and util.root_pattern(".git")(fname)
-			or vim.fn.getcwd()
-	end,
-})
 
 require("lsp_signature").setup()
--- neovim/lua/autocomplete/on_attach.lua
+++ neovim/lua/autocomplete/on_attach.lua
@@ -6,11 +6,38 @@ return function(opts)
 		-- Enable completion triggered by <c-x><c-o>
 		vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 		require("keymaps").lsp_keymaps(opts)(bufnr)
-		require("lsp-inlayhints").on_attach(client, bufnr)
 
-		if client.server_capabilities.documentSymbolProvider then
+		if client.server_capabilities.documentSymbolProvider and client.name ~= "jedi_language_server" then
 			navbuddy.attach(client, bufnr)
 			navic.attach(client, bufnr)
+		end
+
+		if client.name == "basedpyright" then
+			client.server_capabilities.hoverProvider = false
+		end
+
+		if vim.lsp.inlay_hint then
+			vim.lsp.inlay_hint.enable(bufnr, true)
+		end
+
+		if client.name == "jedi_language_server" then
+			client.server_capabilities = {
+				codeActionProvider = true,
+				hoverProvider = true,
+				textDocumentSync = {
+					change = 2,
+					openClose = true,
+					save = {
+						includeText = true,
+					},
+				},
+				workspace = {
+					workspaceFolders = {
+						changeNotifications = true,
+						supported = true,
+					},
+				},
+			}
 		end
 	end
 end
