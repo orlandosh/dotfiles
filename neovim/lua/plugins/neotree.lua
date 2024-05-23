3f81fef652e5944ee35a8ae700387794d2b553ba
orlandosh <84464728+orlandosh@users.noreply.github.com>
2024-05-23 20:09:24 -0300

    update neotree to 0.11

--- neovim/lua/plugins/neotree.lua
+++ neovim/lua/plugins/neotree.lua
@@ -1,14 +1,15 @@
 return function()
-	-- Unless you are still migrating, remove the deprecated commands from v1.x
-	vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-
 	-- If you want icons for diagnostic errors, you'll need to define them somewhere:
-	vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-	vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-	vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-	vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
-	-- this is changed from v1.x, which used the old style of highlight groups
-	-- in the form "LspDiagnosticsSignWarning"
+	vim.diagnostic.config({
+		signs = {
+			text = {
+				[vim.diagnostic.severity.ERROR] = " ",
+				[vim.diagnostic.severity.WARN] = " ",
+				[vim.diagnostic.severity.INFO] = " ",
+				[vim.diagnostic.severity.HINT] = "󰌵",
+			},
+		},
+	})
 
 	require("neo-tree").setup({
 		close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
