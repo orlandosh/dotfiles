1e20b61cdc45cf7ae93b08da51211a1dd89b0a2d
orlandosh <84464728+orlandosh@users.noreply.github.com>
2024-06-14 18:04:17 -0300

    format

--- neovim/lua/settings.lua
+++ neovim/lua/settings.lua
@@ -68,11 +68,11 @@ if vim.g.neovide then
 	G.terminal_color_13 = "#d3869b"
 	G.terminal_color_14 = "#89b482"
 	G.terminal_color_15 = "#a89984"
-	vim.keymap.set("n", "<D-s>", ":w<CR>")   -- Save
-	vim.keymap.set("v", "<D-c>", '"+y')      -- Copy
-	vim.keymap.set("n", "<D-v>", '"+P')      -- Paste normal mode
-	vim.keymap.set("v", "<D-v>", '"+P')      -- Paste visual mode
-	vim.keymap.set("c", "<D-v>", "<C-R>+")   -- Paste command mode
+	vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+	vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+	vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
 	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
 end
 
